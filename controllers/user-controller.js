b9813530c3527514940c15ddfb0725ac236bb3f6
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-28 13:14:02 -0500

    Updated user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -30,4 +30,39 @@ const userController = {
             });
     },
 
-    
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err)
+                res.status(400).json(err);
+            });
+    },
+
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err))
+    },
+
+    deleteUser({ params }, res) {
+        User.findByIdAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+
+};
+
+module.exports = userController;
