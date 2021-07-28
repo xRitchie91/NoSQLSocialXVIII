52c97c3c1083b0d4a9e228c6b29f427172ae0db8
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-28 13:11:05 -0500

    added user controller
    user controller and user ID search added

---
+++ controllers/user-controller.js
@@ -0,0 +1,33 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    
